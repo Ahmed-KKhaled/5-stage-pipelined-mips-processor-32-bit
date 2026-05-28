9effbf9e5926fcbece27bc83f3b6e69d3a75bada
Ahmed khaled <zakreahmed666@gmail.com>
2026-05-28 15:07:02 +0300

    Update mips_pipeline_tb (1).vhd

--- mips_pipeline_tb (1).vhd
+++ mips_pipeline_tb (1).vhd
@@ -1,7 +1,5 @@
--- =============================================================
 --  mips_pipeline_tb.vhd
 --  Comprehensive testbench
--- =============================================================
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
